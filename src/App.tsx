a908de70dd86b4a6cc53ffef5fbe0b42c92dd3a0
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 07:55:11 +0300

    App - self closing tags, atribute names can be diff, fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to React</h1>
-      <p>
-        This is JSX — it looks like HTML, but it's not quite the same. It has
-        its own rules!
-      </p>
-    </div>
+    <>
+      <div>
+        <h1>Welcome to React</h1>
+        <p>
+          This is JSX — it looks like HTML, but it's not quite the same. It has
+          its own rules!
+        </p>
+        {/* self closing tags: */}
+        <img src="" alt="" />
+        <input type="text" />
+        {/* atribute names can be diff: */}
+        <label htmlFor="login">
+          <input id="login" />
+        </label>
+      </div>
+      {/* use react fragment */}
+      <p>second tag</p>
+    </>
   );
 }
 
