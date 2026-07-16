4071ce0938e293240d21711c3477a539cdc44ccb
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 09:40:08 +0300

    destruct Product props

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,12 +4,12 @@ interface ProductProps {
   price: number;
 }
 
-export default function Product(props: ProductProps) {
+export default function Product({ name, imgUrl, price }: ProductProps) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Price: {props.price} credits</p>
-      <img src={props.imgUrl} alt={props.name} width="320" />
+      <h2>{name}</h2>
+      <p>Price: {price} credits</p>
+      <img src={imgUrl} alt={name} width="480" />
     </div>
   );
 }
