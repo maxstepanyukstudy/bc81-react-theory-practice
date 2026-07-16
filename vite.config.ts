960209d0c9e0ddab6c9f462117864f375f26530d
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 19:03:57 +0300

    vite config - Disable sourcemap: true

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  build: {
-    sourcemap: true, // for mentor code reviews
-  },
+  // build: {
+  //   sourcemap: true, // for mentor code reviews
+  // },
 })
