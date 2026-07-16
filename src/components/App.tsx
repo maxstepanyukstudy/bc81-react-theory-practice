19e8a2b8f87f58d7b5350746ed9f9832ba08656f
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 09:20:45 +0300

    refactor - move to Product.tsx Tests.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,6 @@
 import "./App.css";
-import heroImgPath from "./assets/hero.png";
-
-const techName = "React";
-const imgUrl =
-  "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=640";
-
-function Tests() {
-  return (
-    <>
-      <div>
-        <h1>Welcome to {techName}!</h1>
-        <p>
-          This is JSX — it looks like HTML, but it's not quite the same. It has
-          its own rules!
-        </p>
-        {/* self closing tags: */}
-        <img src={heroImgPath} alt="" />
-        <br />
-        <input type="text" />
-        {/* atribute names can be diff: */}
-        <label htmlFor="login">
-          <input id="login" />
-        </label>
-        <img src={imgUrl} alt="Man, field and a mountain" width="640" />
-      </div>
-      {/* use react fragment */}
-      <p>second tag</p>
-    </>
-  );
-}
-
-function Product() {
-  return (
-    <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-}
+import Product from "./Product";
+import Tests from "./Tests";
 
 function App() {
   return (
---
+++ src/components/Tests.tsx
@@ -0,0 +1,30 @@
+import heroImgPath from "../assets/hero.png";
+
+const techName = "React";
+const imgUrl =
+  "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=640";
+
+export default function Tests() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to {techName}!</h1>
+        <p>
+          This is JSX — it looks like HTML, but it's not quite the same. It has
+          its own rules!
+        </p>
+        {/* self closing tags: */}
+        <img src={heroImgPath} alt="" />
+        <br />
+        <input type="text" />
+        {/* atribute names can be diff: */}
+        <label htmlFor="login">
+          <input id="login" />
+        </label>
+        <img src={imgUrl} alt="Man, field and a mountain" width="640" />
+      </div>
+      {/* use react fragment */}
+      <p>second tag</p>
+    </>
+  );
+}
